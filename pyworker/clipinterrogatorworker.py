bfa4b7a70e20ec4b4674e49eb2dbf07de2eb9371
huoju <virushuo@gmail.com>
2022-09-21 02:28:33 -0400

    code refactoring

--- pyworker/clipinterrogatorworker.py
+++ pyworker/clipinterrogatorworker.py
@@ -5,10 +5,10 @@ from torchvision.transforms.functional import InterpolationMode
 from PIL import Image
 
 root_path = os.getcwd()
-sys.path.append(f'{root_path}/CLIP')
+sys.path.append(f"{root_path}/CLIP")
 from CLIP import clip
 
-sys.path.append(f'{root_path}/BLIP')
+sys.path.append(f"{root_path}/BLIP")
 from models.blip import blip_decoder
 
 logging.basicConfig(
@@ -18,28 +18,49 @@ logging.basicConfig(
     filemode="w",
 )
 
-models = ['ViT-L/14']
+models = ["ViT-L/14"]
 blip_image_eval_size = 384
 
+
 def load_list(filename):
-    with open(filename, 'r', encoding='utf-8', errors='replace') as f:
+    with open(filename, "r", encoding="utf-8", errors="replace") as f:
         items = [line.strip() for line in f.readlines()]
     return items
+
 
 class Worker:
     def __init__(self, deviceid, args):
         self.device = torch.device(deviceid)
-        data_path = f'{root_path}/data/'
-        self.artists = load_list(os.path.join(data_path, 'artists.txt'))
-        self.flavors = load_list(os.path.join(data_path, 'flavors.txt'))
-        self.mediums = load_list(os.path.join(data_path, 'mediums.txt'))
-        self.movements = load_list(os.path.join(data_path, 'movements.txt'))
-        
-        sites = ['Artstation', 'behance', 'cg society', 'cgsociety', 'deviantart', 'dribble', 'flickr', 'instagram', 'pexels', 'pinterest', 'pixabay', 'pixiv', 'polycount', 'reddit', 'shutterstock', 'tumblr', 'unsplash', 'zbrush central']
+        data_path = f"{root_path}/data/"
+        self.artists = load_list(os.path.join(data_path, "artists.txt"))
+        self.flavors = load_list(os.path.join(data_path, "flavors.txt"))
+        self.mediums = load_list(os.path.join(data_path, "mediums.txt"))
+        self.movements = load_list(os.path.join(data_path, "movements.txt"))
+
+        sites = [
+            "Artstation",
+            "behance",
+            "cg society",
+            "cgsociety",
+            "deviantart",
+            "dribble",
+            "flickr",
+            "instagram",
+            "pexels",
+            "pinterest",
+            "pixabay",
+            "pixiv",
+            "polycount",
+            "reddit",
+            "shutterstock",
+            "tumblr",
+            "unsplash",
+            "zbrush central",
+        ]
         self.trending_list = [site for site in sites]
-        self.trending_list.extend(["trending on "+site for site in sites])
-        self.trending_list.extend(["featured on "+site for site in sites])
-        self.trending_list.extend([site+" contest winner" for site in sites])
+        self.trending_list.extend(["trending on " + site for site in sites])
+        self.trending_list.extend(["featured on " + site for site in sites])
+        self.trending_list.extend([site + " contest winner" for site in sites])
 
     def loadmodel(self):
         self.clipmodels = {}
@@ -48,12 +69,17 @@ class Worker:
             model, preprocess = clip.load(model_name, device=self.device, jit=True)
             model = model.to(self.device)
             model.eval()
-            self.clipmodels[model_name] = {"model": model,"preprocess": preprocess}
+            self.clipmodels[model_name] = {"model": model, "preprocess": preprocess}
             print("model loaded: ", model_name)
 
         print("load blip model")
-        blip_model_url = f'{root_path}/models/model_base_caption.pth'        
-        blip_model = blip_decoder(pretrained=blip_model_url, med_config=f'{root_path}/BLIP/configs/med_config.json',image_size=blip_image_eval_size, vit='base')
+        blip_model_url = f"{root_path}/models/model_base_caption.pth"
+        blip_model = blip_decoder(
+            pretrained=blip_model_url,
+            med_config=f"{root_path}/BLIP/configs/med_config.json",
+            image_size=blip_image_eval_size,
+            vit="base",
+        )
         self.blip_model = blip_model.to(self.device)
         self.blip_model.eval()
 
@@ -63,26 +89,40 @@ class Worker:
         if prevoutput != None:
             data = prevoutput.Data
         elif inputtask.Data != None:
-            data = inputtask.Data 
+            data = inputtask.Data
         if data == None:
             return "ERR_NO_INPUT_IMAGE_DATA", "", {}, {}
 
         dataStream = io.BytesIO(data)
-        pilimage = Image.open(dataStream).convert('RGB')
-        outputdesc = self.interrogate('ViT-L/14',pilimage)
+        pilimage = Image.open(dataStream).convert("RGB")
+        outputdesc = self.interrogate("ViT-L/14", pilimage)
         args = ""
         data = str.encode(outputdesc, "utf-8")
         return "", "text/plain", data, inputsettings
 
     def generate_caption(self, image):
-        gpu_image = transforms.Compose([
-            transforms.Resize((blip_image_eval_size, blip_image_eval_size), interpolation=InterpolationMode.BICUBIC),
-            transforms.ToTensor(),
-            transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-        ])(image).unsqueeze(0).to(self.device)
-    
+        gpu_image = (
+            transforms.Compose(
+                [
+                    transforms.Resize(
+                        (blip_image_eval_size, blip_image_eval_size),
+                        interpolation=InterpolationMode.BICUBIC,
+                    ),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        (0.48145466, 0.4578275, 0.40821073),
+                        (0.26862954, 0.26130258, 0.27577711),
+                    ),
+                ]
+            )(image)
+            .unsqueeze(0)
+            .to(self.device)
+        )
+
         with torch.no_grad():
-            caption = self.blip_model.generate(gpu_image, sample=False, num_beams=3, max_length=20, min_length=5)
+            caption = self.blip_model.generate(
+                gpu_image, sample=False, num_beams=3, max_length=20, min_length=5
+            )
         return caption[0]
 
     def interrogate(self, modelname, image):
@@ -90,50 +130,57 @@ class Worker:
         if len(models) == 0:
             print(f"\n\n{caption}")
             return
-    
+
         table = []
-        bests = [[('',0)]]*5
+        bests = [[("", 0)]] * 5
         clipmodel = self.clipmodels[modelname]
-    
+
         preprocess = clipmodel["preprocess"]
         model = clipmodel["model"]
-    
+
         images = preprocess(image).unsqueeze(0).cuda()
         with torch.no_grad():
             image_features = model.encode_image(images).float()
         image_features /= image_features.norm(dim=-1, keepdim=True)
-    
+
         ranks = [
             self.rank(model, image_features, self.mediums),
-            self.rank(model, image_features, ["by "+artist for artist in self.artists]),
+            self.rank(
+                model, image_features, ["by " + artist for artist in self.artists]
+            ),
             self.rank(model, image_features, self.trending_list),
             self.rank(model, image_features, self.movements),
-            self.rank(model, image_features, self.flavors, top_count=3)
+            self.rank(model, image_features, self.flavors, top_count=3),
         ]
-    
+
         for i in range(len(ranks)):
             confidence_sum = 0
             for ci in range(len(ranks[i])):
                 confidence_sum += ranks[i][ci][1]
             if confidence_sum > sum(bests[i][t][1] for t in range(len(bests[i]))):
                 bests[i] = ranks[i]
-    
+
         ranktab = ""
-        columns=["Medium", "Artist", "Trending", "Movement", "Flavors"]
+        columns = ["Medium", "Artist", "Trending", "Movement", "Flavors"]
         idx = 0
         for r in ranks:
-            ranktab = ranktab + columns[idx] + ":" + ' '.join([f"{x[0]} ({x[1]:0.1f}%)" for x in r]) + " "
+            ranktab = (
+                ranktab
+                + columns[idx]
+                + ":"
+                + " ".join([f"{x[0]} ({x[1]:0.1f}%)" for x in r])
+                + " "
+            )
             idx = idx + 1
-    
-        flaves = ', '.join([f"{x[0]}" for x in bests[4]])
+
+        flaves = ", ".join([f"{x[0]}" for x in bests[4]])
         medium = bests[0][0][0]
         outputdesc = ""
         if caption.startswith(medium):
             outputdesc = f"\n\n{caption} {bests[1][0][0]}, {bests[2][0][0]}, {bests[3][0][0]}, {flaves}"
         else:
-            outputdesc =f"\n\n{caption} {medium} {bests[1][0][0]}, {bests[2][0][0]}, {bests[3][0][0]}, {flaves}"
+            outputdesc = f"\n\n{caption} {medium} {bests[1][0][0]}, {bests[2][0][0]}, {bests[3][0][0]}, {flaves}"
         return ranktab + outputdesc
-
 
     def rank(self, model, image_features, text_array, top_count=1):
         top_count = min(top_count, len(text_array))
@@ -141,14 +188,19 @@ class Worker:
         with torch.no_grad():
             text_features = model.encode_text(text_tokens).float()
         text_features /= text_features.norm(dim=-1, keepdim=True)
-    
+
         similarity = torch.zeros((1, len(text_array))).to(self.device)
         for i in range(image_features.shape[0]):
-            similarity += (100.0 * image_features[i].unsqueeze(0) @ text_features.T).softmax(dim=-1)
+            similarity += (
+                100.0 * image_features[i].unsqueeze(0) @ text_features.T
+            ).softmax(dim=-1)
         similarity /= image_features.shape[0]
-    
-        top_probs, top_labels = similarity.cpu().topk(top_count, dim=-1)  
-        return [(text_array[top_labels[0][i].numpy()], (top_probs[0][i].numpy()*100)) for i in range(top_count)]
-    
+
+        top_probs, top_labels = similarity.cpu().topk(top_count, dim=-1)
+        return [
+            (text_array[top_labels[0][i].numpy()], (top_probs[0][i].numpy() * 100))
+            for i in range(top_count)
+        ]
+
     def settingsToOutput(self, settings, finalsettings):
         return ""
---
+++ pyworker/worker.py
@@ -0,0 +1,192 @@
+import os, sys, io, random, ssl, time, json
+import importlib
+import logging, configparser
+import threading
+from threading import Lock
+import pika
+from PIL import Image
+import df_pb2
+
+tasklock = Lock()
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    stream=sys.stdout,
+    level=logging.WARNING,
+    filemode="w",
+)
+
+
+class ThreadedConsumer(threading.Thread):
+    def __init__(self, tid, taskqueuename, config):
+        threading.Thread.__init__(self)
+        self.tid = tid
+        taskqueuekey = tid
+        amqpurl = config["AMQP_URL"]
+        parameters = pika.URLParameters(amqpurl)
+
+        if "TLS_CA_CERT" in config:
+            context = ssl.create_default_context(cafile=config["TLS_CA_CERT"])
+            context.load_cert_chain(config["TLS_CLIENT_CERT"], config["TLS_CLIENT_KEY"])
+            ssl_options = pika.SSLOptions(context, "localhost")
+            parameters.ssl_options = ssl_options
+
+        self.connection = pika.BlockingConnection(parameters)
+        self.channel = self.connection.channel()
+        self.channel.basic_qos(prefetch_count=1)
+        qname = taskqueuename + "." + taskqueuekey
+        print("waiting tasks from {}".format(qname))
+        self.channel.basic_consume(
+            queue=qname, on_message_callback=self.callback, auto_ack=False
+        )
+        threading.Thread(
+            target=self.channel.basic_consume(
+                queue=qname, on_message_callback=self.callback, auto_ack=False
+            )
+        )
+
+    def callback(self, channel, method, properties, body):
+        print(
+            "[x] Received from {}, body length {}".format(method.routing_key, len(body))
+        )
+        with tasklock:
+            print(" [*] task start...")
+
+            task = df_pb2.Task()
+            task.ParseFromString(body)
+
+            err, inputId, mime, data, args = taskRunner(task)
+            if err == "":
+                print("AI task Succ, send ack.", inputId, task.TaskId)
+                pbdata, nexttask = repacktask(inputId, mime, data, task, args)
+                # find next inputtask
+                r = publish(pbdata, channel, nexttask.Name)
+                if r == "":
+                    logging.debug("AI task Succ, send ack.")
+                    channel.basic_ack(method.delivery_tag)
+                else:
+                    logging.debug("AI task Err:", r)
+                    channel.basic_ack(method.delivery_tag)
+            else:
+                print("AI task failure, send nack.", inputId, task.TaskId)
+                print(err)
+                logging.debug("AI task failure, send nack")
+                # channel.basic_nack(method.delivery_tag)
+                channel.basic_ack(method.delivery_tag)
+
+    def run(self):
+        print("start thread:", self.tid)
+        self.channel.start_consuming()
+
+
+def loadconf(loadworkername):
+    config = configparser.ConfigParser()
+    configfilename = "configs/{}_config.ini".format(loadworkername)
+    print("loading config:", configfilename)
+    config.read_file(open(configfilename, "r"))
+    return config
+
+
+def taskRunner(task):
+    outputlen = len(task.OutputList)
+    if outputlen >= len(task.InputList):
+        return
+
+    inputtask = task.InputList[outputlen]
+    prevoutput = None
+    if outputlen > 0:
+        prevoutput = task.OutputList[outputlen - 1]
+
+    ainame = inputtask.Name
+    if ainame == workername:
+        inputsettings = json.loads(inputtask.Settings)
+        # r, mime, data, finalsettings = worker.work(inputsettings)
+        r, mime, data, finalsettings = worker.work(inputtask, prevoutput)
+        print("image len:", len(data))
+        args = worker.settingsToOutput(inputsettings, finalsettings)
+        return r, inputtask.InputId, mime, data, bytes(args, "utf-8")
+    else:
+        # not success, return errr and nack
+        return "ERR_NOT_SUPPORT_MODEL", "", "", [], ""
+
+
+def publish(result, channel, taskname):
+    l = taskname.split(".")
+    if len(l) == 3:  # with route key
+        key = l[2]
+        exchange = l[0] + "." + l[1]
+        channel.basic_publish(exchange=exchange, routing_key=key, body=result)
+        return ""
+    elif len(l) == 2:  # no special routekey, use all as route key
+        exchange = l[0] + "." + l[1]
+        channel.basic_publish(exchange=exchange, routing_key="all", body=result)
+        return ""
+    else:
+        return "ERR_INVAILD_TASKNAME"
+
+
+# def fakeaiwork(settings):
+#    print("*** fake ai working***")
+#    print(settings)
+#    time.sleep(5)
+#    r = random.random()
+#    if r < 0.5:
+#        #fake result image
+#        filename = "output.png"
+#        image = Image.open(filename)
+#        img_byte_arr = io.BytesIO()
+#        image.save(img_byte_arr, format='PNG')
+#        img_byte_arr = img_byte_arr.getvalue()
+#        return "", "image/png", img_byte_arr
+#    return "ERR_AIWORK_FAILURE", "", []
+
+
+def repacktask(inputId, mime, data, task, args):
+    r = df_pb2.Output()
+    r.InputId = inputId
+    r.Version = 1
+    r.ProducerName = cfg["WORKER"]["NAME"]
+    r.ProducerSign = "mysig_taskid_userid"
+    r.MimeType = mime
+    r.Data = data
+    r.Args = args
+
+    task.OutputList.append(r)
+
+    nexttask = task.InputList[len(task.OutputList)]
+    output = task.SerializeToString()
+    return output, nexttask
+
+
+if __name__ == "__main__":
+    global workername, cfg, worker
+    loadworkername = "sd14"
+    if len(sys.argv) > 1:
+        loadworkername = sys.argv[1]
+
+    print("load worker :", loadworkername)
+    cfg = loadconf(loadworkername)
+
+    workermodule = importlib.import_module(loadworkername + "worker")
+    worker = workermodule.Worker("cuda:0", {})
+    worker.loadmodel()
+    queueconfig = cfg["QUEUE"]
+    workername = queueconfig["WORKER_NAME"]
+    workerkeys = queueconfig["WORKER_KEYS"]
+
+    keylist = workerkeys.split(",")
+
+    try:
+        for key in keylist:
+            key = key.strip()
+            td = ThreadedConsumer(key, workername, queueconfig)
+            td.start()
+        print("* Worker waiting for tasks. To exit press CTRL+C")
+
+    except KeyboardInterrupt:
+        print("* Exiting")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
