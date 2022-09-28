5527a29da58db2b41af34a7b6933d0feae717195
huoju <virushuo@gmail.com>
2022-09-28 01:31:01 -0400

    use prompt + args as the output filename

--- pkg/worker/discord.go
+++ pkg/worker/discord.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	dfpb "github.com/huo-ju/dfserver/pkg/pb"
@@ -50,12 +51,20 @@ func (f *ProcessDiscordWorker) Work(outputList []*dfpb.Output, lastinput *dfpb.I
 
 	//bot response images
 	r := bytes.NewReader(lastoutput.Data)
+	filename := ""
 	for _, o := range outputList {
 		content += fmt.Sprintf("!dream %s | by %s\r", string(o.Args), *o.ProducerName)
+		filename += string(o.Args)
 	}
+
+	filename = strings.Replace(filename, " ", "_", -1)
+	if len(filename) > 250 { //max filename length 250
+		filename = filename[len(filename)-250:]
+	}
+
 	msg := &discordgo.MessageSend{
 		Content:   content,
-		File:      &discordgo.File{Name: "output.png", Reader: r},
+		File:      &discordgo.File{Name: filename + ".png", Reader: r},
 		Reference: ref,
 	}
 
