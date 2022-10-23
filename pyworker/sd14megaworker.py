178bd1ff1e56712d109b517046666e512c2a6721
huoju <virushuo@gmail.com>
2022-10-23 19:27:31 -0400

    bug fixed. output strength value format

--- pyworker/sd14megaworker.py
+++ pyworker/sd14megaworker.py
@@ -238,8 +238,8 @@ class Worker:
             output = output + " -S " + str(settings["seed"])
         if "init_image_url" in settings and settings["init_image_url"] != "":
             output = output + " -IMG " + str(settings["init_image_url"])
-        if "strength" in settings and settings["strength"] != "":
-            output = output + " -E" + str(settings["strength"])
+        if "strength" in settings and settings["strength"] != "" and settings["strength"] != "0":
+            output = output + " -E " + str(settings["strength"])
         elif "seed" in finalsettings and finalsettings["seed"] != 0:
             output = output + " -S " + str(finalsettings["seed"])
 
